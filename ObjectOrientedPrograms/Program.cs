7f0f836eb6a3f75d693002aa4bcdb24e39b68448
Your Name <you@example.com>
2023-02-22 15:38:01 +0530

    Adding a welcome message

---
+++ ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Object oriented Programs");
+
+            Console.ReadLine();
+        }
+    }
+}

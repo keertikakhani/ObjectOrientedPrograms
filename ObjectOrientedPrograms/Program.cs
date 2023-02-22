86c43710c32c64cd3e7671db462efc8864a645f4
Your Name <you@example.com>
2023-02-22 18:06:32 +0530

    Creating a json file and inventory management of different types of rice, wheat and pulses

--- ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/Program.cs
@@ -8,6 +8,38 @@ namespace ObjectOrientedPrograms
         {
             Console.WriteLine("Welcome to Object oriented Programs");
 
+            string path = "C:\\Users\\rishi\\OneDrive\\Desktop\\New\\ObjectOrientedPrograms\\ObjectOrientedPrograms\\Inventory.json";
+            FetchInventory obj = new FetchInventory();
+
+            InventoryDetails data = obj.Read(path);
+
+            Console.WriteLine("Types of Rice");
+            for (int i = 0; i < data.typesOfRice.Count; i++)
+            {
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine(data.typesOfRice[i].weight);
+                Console.WriteLine(data.typesOfRice[i].price);
+            }
+            Console.WriteLine("**********************");
+
+            Console.WriteLine("Types of Pulses");
+            for (int i = 0; i < data.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(data.typesOfPulses[i].name);
+                Console.WriteLine(data.typesOfPulses[i].weight);
+                Console.WriteLine(data.typesOfPulses[i].price);
+            }
+            Console.WriteLine("**********************");
+
+            Console.WriteLine("Types of Wheat");
+            for (int i = 0; i < data.typesOfWheat.Count; i++)
+            {
+                Console.WriteLine(data.typesOfWheat[i].name);
+                Console.WriteLine(data.typesOfWheat[i].weight);
+                Console.WriteLine(data.typesOfWheat[i].price);
+            }
+            Console.WriteLine("**********************");
+
             Console.ReadLine();
         }
     }
